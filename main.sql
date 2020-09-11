0f8c7753058995742f0b494ed12f9426eb08a5d1
Dmitry Barashev <dmitry@barashev.net>
2020-09-11 12:52:17 +0000

    чуток sql кода

--- main.sql
+++ main.sql
@@ -1,1 +1,10 @@
 SELECT 2+2;
+-- что это
+
+SELECT 'Hello World!';
+
+drop table if exists Foo;
+CREATE TABLE Foo(id int primary key, name text);
+insert into Foo(id, name) values (1, 'John');
+
+select * from Foo;
