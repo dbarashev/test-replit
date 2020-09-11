4a85f6116c82ccf39bb8bfa9dce763175123a5b2
Dmitry Barashev <dmitry@barashev.net>
2020-09-11 12:54:13 +0000

    дбавили Bar

--- main.sql
+++ main.sql
@@ -8,3 +8,8 @@ CREATE TABLE Foo(id int primary key, name text);
 insert into Foo(id, name) values (1, 'John');
 
 select * from Foo;
+
+create table Bar(foo_id INT, value INT);
+INSERT INTO Bar(foo_id, value) VALUES (1, 10);
+
+SELECT * FROM Bar;
